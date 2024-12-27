f97ab65d78a4c41a14d93a057bbd9dcc94fb052e
Azamat <h7559006@gmai.com>
2024-12-27 12:32:07 +0500

    Complete Prisma seeding at half

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { categories, ingredients, products } from "./constants";
+import { prisma } from "./prisma-client";
+import { hashSync } from "bcrypt";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User Test",
+        email: "user@test.kz",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "Admin Test",
+        email: "admin@test.kz",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+
+  await prisma.category.createMany({
+    data: categories,
+  });
+
+  await prisma.ingredient.createMany({
+    data: ingredients,
+  });
+
+  await prisma.product.createMany({
+    data: products,
+  });
+}
+
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
