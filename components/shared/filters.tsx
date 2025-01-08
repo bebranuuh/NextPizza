eb7cba5cd82389cbd71d193cca773ba1c33b4663
Azamat <h7559006@gmai.com>
2025-01-08 12:16:49 +0500

    Made Skeletons for checkboxFiltersGroup

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -12,7 +12,7 @@ interface Props {
 }
 
 export const Filters: React.FC<Props> = ({ className }) => {
-  const { ingredients } = useFilterIngredients();
+  const { ingredients, loading } = useFilterIngredients();
 
   const items = ingredients.map((ingredient) => ({
     value: String(ingredient.id),
@@ -44,6 +44,7 @@ export const Filters: React.FC<Props> = ({ className }) => {
         limit={6}
         defaultItems={items.slice(0, 6)}
         items={items}
+        loading={loading}
       />
     </div>
   );
--- hooks/useFilterIngredients.ts
+++ hooks/useFilterIngredients.ts
@@ -4,23 +4,28 @@ import React from "react";
 
 interface ReturnProps {
   ingredients: Ingredient[];
+  loading: boolean;
 }
 
 export const useFilterIngredients = (): ReturnProps => {
   const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
     async function fethcIngredients() {
       try {
+        setLoading(true);
         const ingredients = await Api.ingredients.getAll();
         setIngredients(ingredients);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     }
 
     fethcIngredients();
   }, []);
 
-  return { ingredients };
+  return { ingredients, loading };
 };
